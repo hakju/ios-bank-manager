abcda85e09de1a2929095c2af5b2ccfb32e180fb
wonhee <wonhee010@gmail.com>
2021-01-14 02:04:25 +0900

    feat: add button action closure & add stackView to view

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -7,12 +7,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let addAction: ((_ sender: UIButton) -> Void) = {_ in
+        print("add")
+    }
+    private let resetAction: ((_ sender: UIButton) -> Void) = {_ in
+        print("reset")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpButtons()
     }
-
-
+    
+    // MARK: - set up UI
+    private func setUpButtons() {
+        let buttonStack = StackWithButtons(touchAdd: addAction, touchReset: resetAction)
+        self.view.addSubview(buttonStack)
+        buttonStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        buttonStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        buttonStack.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        buttonStack.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
 }
 
