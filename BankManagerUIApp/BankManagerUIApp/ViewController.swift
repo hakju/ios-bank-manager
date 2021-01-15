481c97b92f2ef469377bd1a7f529ae4619d004f3
wonhee <wonhee010@gmail.com>
2021-01-15 15:58:59 +0900

    feat: add viewController

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -1,18 +1,83 @@
 //
 //  BankManagerUIApp - ViewController.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
-// 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: UI property
+    private lazy var timerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.text = "업무시간 - 00:00:000"
+        return label
+    }()
+    private lazy var buttonStack: UIStackView = {
+        let stackView = StackWithButtons(touchAdd: addAction, touchReset: resetAction)
+        return stackView
+    }()
+    private lazy var judgeCustomersRow: BankRowView = {
+        let view = BankRowView(rowTitle: "심사중", rowColor: .systemOrange)
+        return view
+    }()
+    private lazy var workingCustomersRow: BankRowView = {
+        let view = BankRowView(rowTitle: "업무중", rowColor: .systemGreen)
+        return view
+    }()
+    private lazy var waitingCustomersRow: BankRowView = {
+        let view = BankRowView(rowTitle: "대기중", rowColor: .systemPurple)
+        return view
+    }()
+    
+    // MARK: button closures
+    private let addAction: ((_ sender: UIButton) -> Void) = {_ in
+        print("add")
+    }
+    private let resetAction: ((_ sender: UIButton) -> Void) = {_ in
+        print("reset")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpButtons()
+        setUpTimerLabel()
+        setUpBankCustomersRow()
+        judgeCustomersRow.addSubviewInScroll(CustomerView(customer: try! Customer(waitingNumber: 1)))
+//        judgeCustomersRow.addSubviewInScroll(CustomerView(customer: try! Customer(waitingNumber: 2)))
+    }
+    
+    // MARK: - set up UI
+    private func setUpButtons() {
+        self.view.addSubview(buttonStack)
+        buttonStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        buttonStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        buttonStack.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        buttonStack.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private func setUpTimerLabel() {
+        self.view.addSubview(timerLabel)
+        timerLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        timerLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
     }
 
-
+    private func setUpBankCustomersRow() {
+        let containerStack = UIStackView()
+        containerStack.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(containerStack)
+        containerStack.axis = .vertical
+        containerStack.distribution = .fillEqually
+        containerStack.topAnchor.constraint(equalTo: self.timerLabel.bottomAnchor).isActive = true
+        containerStack.bottomAnchor.constraint(equalTo: self.buttonStack.topAnchor).isActive = true
+        containerStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        containerStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+        containerStack.addArrangedSubview(judgeCustomersRow)
+        containerStack.addArrangedSubview(workingCustomersRow)
+        containerStack.addArrangedSubview(waitingCustomersRow)
+    }
 }
 
