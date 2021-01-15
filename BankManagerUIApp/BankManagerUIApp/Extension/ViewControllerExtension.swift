423e0facf6a96603d49d70a14c6a670541cfc7d0
wonhee <wonhee010@gmail.com>
2021-01-15 22:00:05 +0900

    feat: add show error func

--- BankManagerUIApp/BankManagerUIApp/Extension/ViewControllerExtension.swift
+++ BankManagerUIApp/BankManagerUIApp/Extension/ViewControllerExtension.swift
@@ -5,4 +5,21 @@
 //  Created by Wonhee on 2021/01/15.
 //
 
-import Foundation
+import UIKit
+
+extension ViewController {
+    func showError(_ error: Error, okAction: ((UIAlertAction) -> Void)?) {
+        var errorMessage: String
+        if let bankError = error as? BankError {
+            errorMessage = bankError.localizedDescription
+        } else {
+            errorMessage = BankError.unknown.localizedDescription
+        }
+        
+        let errorAlert = UIAlertController(title: nil, message: errorMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        errorAlert.addAction(okAction)
+        
+        self.present(errorAlert, animated: true, completion: nil)
+    }
+}
