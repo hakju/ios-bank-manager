73d1ded2b51df8d9f014618f6c0ce2b44eecf997
wonhee <wonhee010@gmail.com>
2021-01-15 14:58:55 +0900

    feat: change location files

--- BankManagerUIApp/BankManagerUIApp/Data/Bank.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Bank.swift
-//  BankManagerUIApp
-//
-//  Created by Wonhee on 2021/01/14.
-//
-
-import Foundation
--- BankManagerUIApp/BankManagerUIApp/Data/Banker.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Banker.swift
-//  BankManagerUIApp
-//
-//  Created by Wonhee on 2021/01/14.
-//
-
-import Foundation
