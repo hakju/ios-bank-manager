743dca03863179636a1594837b4c36a369fb924d
wonhee <wonhee010@gmail.com>
2021-01-15 21:52:45 +0900

    feat: divide Error Type

--- CommonData/Error/BankError.swift
+++ CommonData/Error/BankError.swift
@@ -7,25 +7,6 @@
 
 import Foundation
 
-enum InputError: Error {
-    case input
-    case number
-    case mismatchNumber
-}
-
-extension InputError: LocalizedError {
-    var errorDescription: String? {
-        switch self {
-        case .input:
-            return "입력을 받지 못했습니다.\n다시 시도해 주세요."
-        case .number:
-            return "숫자를 입력해주세요."
-        case .mismatchNumber:
-            return "알맞은 숫자를 입력해주세요."
-        }
-    }
-}
-
 enum BankError: Error {
     case typeRandomElement
     case close
